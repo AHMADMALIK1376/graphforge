9ad69f8afd5ecf6a6ee57469f128fb85548f4455
Muhammad Ahmad Malik <ahmadmalik1376@gmail.com>
2026-05-18 04:29:02 +0500

    ✅ Working GraphForge MVP - Bar, Pie, Radar charts with export

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import GraphCalculator from "./pages/GraphCalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <GraphCalculator />
+    </div>
+  );
+}
+
+export default App;
